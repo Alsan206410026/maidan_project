4b906b2eb8fdb1c1b240f8d2dcbb82b96b831a4b
Alsan Pokharel <alsan206410026@example.com>
2026-07-19 13:05:56 +0545

    search

--- backend/controllers/venueController.js
+++ backend/controllers/venueController.js
@@ -14,6 +14,47 @@ const getVenues = async (req, res) => {
     }
 };
 
+// Get a venue by ID 
+const getVenueById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const venue = await Venue.findById(id);
+        if (!venue) {
+            return res.status(404).json({
+                message: "Venue not found",
+            });
+        }
+        return res.status(200).json(venue);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+};
+
+//Search Venues by name or category or price or location
+const SearchVenues = async (req, res) => {
+    const { query } = req.query;
+    try {
+        const venues = await Venue.find({
+            $or: [
+                { name: { $regex: query, $options: "i" } },
+                { category: { $regex: query, $options: "i" } },
+                { price: { $regex: query, $options: "i" } },
+                { location: { $regex: query, $options: "i" } }
+            ]
+        });
+        return res.status(200).json(venues);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+};
+
+
 // Create a new venue
 const createVenue = async (req, res) => {
     const { name, slug, description, category, price, status } = req.body;
@@ -46,12 +87,58 @@ const createVenue = async (req, res) => {
     }
 };
 
-// // Get a venue by searching name,location,category,price,stockQuantity,status,images
-// const getVenueBySearch = async (req, res) => {
-//     const { id } = req.params;
+// Update a venue
+const updateVenue = async (req, res) => {
+    const { name, slug, description, category, price, status } = req.body;
+    const venue = await Venue.findById(req.params.id);
+    if(venue){
+        venue.name = name || venue.name;
+        venue.slug = slug || venue.slug;
+        venue.description = description || venue.description;
+        venue.category = category || venue.category;
+        venue.price = price || venue.price;
+        venue.status = status || venue.status;
+        if (req.file) {
+            const result = await cloudinary.uploader.upload(req.file.path);
+            venue.images = result.secure_url; // Update the image URL in the database
+        }
+        await venue.save();
+        return res.status(200).json(venue);
+    }
+    return res.status(404).json({
+        message: "Venue not found",
+    });
+};
+
+// Delete a venue
+const deleteVenue = async (req, res) => {
+    try {
+        const venue = await Venue.findByIdAndDelete(req.params.id);
+
+        if (!venue) {
+            return res.status(404).json({
+                message: "Venue not found",
+            });
+        }
+
+        return res.status(200).json({
+            message: "Venue removed successfully",
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+};
+
 
 module.exports = {
     getVenues,
     createVenue,
-    // getVenueBySearch
+    getVenueById,
+    updateVenue,
+    deleteVenue,
+    SearchVenues
 };
--- backend/routes/venueRoutes.js
+++ backend/routes/venueRoutes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {getVenues, createVenue} = require("../controllers/venueController.js");
+const {getVenues, createVenue,getVenueById,updateVenue,deleteVenue , SearchVenues} = require("../controllers/venueController.js");
 const { protect } = require("../middleware/authmiddleware.js");
 const { admin } = require("../middleware/adminMiddleware.js");
 const  upload  = require("../middleware/venuemiddleware.js");
 
 
 router.route("/").get(getVenues).post(protect, admin, upload.single("image"), createVenue);
-// router.route("/:id").get(getVenueById).put(protect, admin, updateVenue).delete(protect, admin, deleteVenue);
+router.get("/search", SearchVenues); 
+router.route("/:id").get(getVenueById).put(protect, admin, updateVenue).delete(protect, admin, deleteVenue);
+
 
 
 module.exports = router;
