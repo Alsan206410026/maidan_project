b10140937165da18ec99f616178157fe524de199
Alsan Pokharel <alsan206410026@example.com>
2026-08-07 04:18:19 +0545

    socket connection frontend

---
+++ client/src/context/SocketContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+export const SocketContext = createContext();
+
+ const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    // Create socket connection
+    const newSocket = io("http://localhost:5001", {
+      withCredentials: true,
+    });
+
+    setSocket(newSocket);
+
+    // Cleanup when provider unmounts
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+module.exports = {
+  SocketProvider,
+};
